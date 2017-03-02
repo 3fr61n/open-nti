d7482496554d1aac05f22c3c03a759c9cd5bbc7c
3fr61n <efrain.gonzalez@gmail.com>
2017-03-02 13:27:56 +0100

    add supervisor to input-xflow plugin in order to monitor and restart nfacctd in case of issues

--- plugins/input-xflow/templates/start-input-xflow.sh
+++ plugins/input-xflow/templates/start-input-xflow.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cp /data/templates/nfacctd.tmpl /source/nfacctd.config
-/usr/sbin/nfacctd -f /source/nfacctd.config >>/var/log/nfacctd.log 2>&1 & 
-
 mkdir -p /opt/telegraf/config
 opennti=$(nslookup opennti | grep Address: | grep -v '#53' | awk '{print $2}')
 sed s/opennti/$opennti/g /data/templates/telegraf.tmpl > /opt/telegraf/config/telegraf.conf
